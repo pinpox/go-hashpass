4a22da827525d6aa34a70dac711e4e39ff2a386f
Pablo <pablo1@mailbox.org>
2018-03-26 22:36:09 +0200

    added cli and help text

--- go-hashpass.go
+++ go-hashpass.go
@@ -4,14 +4,21 @@ import (
 	"crypto/sha256"
 	"encoding/base64"
 	"fmt"
+	"github.com/urfave/cli"
+	"log"
+	"os"
+	"time"
 )
 
-func main() {
+func generatePass(key string, input ...string) string {
 
-	key := "mykey"
-	domain := "play.golang.org"
+	data := ""
 
-	secret := []byte(domain + "/" + key)
+	for _, v := range input {
+		data += v + "/"
+	}
+
+	secret := []byte(data + key)
 
 	for i := 0; i < 1<<16; i++ {
 		sum := sha256.Sum256(secret)
@@ -19,7 +26,66 @@ func main() {
 	}
 
 	password := base64.StdEncoding.EncodeToString(secret)[:16]
-	fmt.Println(password)
+	return password
+}
+
+func main() {
+
+	// key := "mykey"
+	// domain := "play.golang.org"
 	// fmt.Println("OIs/gtSGGsuWXLDJ") // expected
 
+	var key string
+	var domain string
+	var version string
+	app := cli.NewApp()
+	app.Name = "go-hashpass"
+	app.Compiled = time.Now()
+	app.Usage = "A hashpass-compatible stateless password manager/genrator"
+	app.Description = "A hashpass-compatible stateless password manager/genrator"
+	app.HideVersion = true
+	app.Authors = []cli.Author{
+		cli.Author{
+			Name:  "Pablo Ovelleiro",
+			Email: "pablo1@mailbox.org",
+		},
+	}
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "key, k",
+			Usage:       "Your secret key",
+			Destination: &key,
+		},
+
+		cli.StringFlag{
+			Name:        "domain, d",
+			Usage:       "The domain to use",
+			Destination: &domain,
+		},
+		cli.StringFlag{
+			Name:        "vesion, v",
+			Usage:       "The password version",
+			Value:       "",
+			Destination: &version,
+		},
+	}
+
+	app.Action = func(c *cli.Context) error {
+		if key != "" && domain != "" {
+			if version != "" {
+				fmt.Println(generatePass(key, domain, version))
+			} else {
+				fmt.Println(generatePass(key, domain))
+			}
+		} else {
+			cli.ShowAppHelp(c)
+		}
+		return nil
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
