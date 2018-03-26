e2dae3517174bd4a9b84153fc87a4135e25900c3
Pablo <pablo1@mailbox.org>
2018-03-26 23:45:09 +0200

    first working version

--- go-hashpass.go
+++ go-hashpass.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"github.com/urfave/cli"
 	"log"
+	"net/url"
 	"os"
 	"time"
 )
@@ -29,19 +30,36 @@ func generatePass(key string, input ...string) string {
 	return password
 }
 
+func stripUrl(link string) string {
+
+	u, err := url.Parse(link)
+
+	// Check for missing protocol
+	if u.Scheme == "" {
+		link = "http://" + link
+		u, err = url.Parse(link)
+	}
+
+	if err != nil {
+		fmt.Println("Failed to extract url. Maybe use --nostrip to use it as is?")
+		log.Fatal(err)
+	}
+
+	return u.Host
+}
+
 func main() {
 
-	// key := "mykey"
-	// domain := "play.golang.org"
-	// fmt.Println("OIs/gtSGGsuWXLDJ") // expected
-
 	var key string
-	var domain string
+	var mUrl string
 	var version string
+	var nostrip bool
+
 	app := cli.NewApp()
 	app.Name = "go-hashpass"
 	app.Compiled = time.Now()
 	app.Usage = "A hashpass-compatible stateless password manager/genrator"
+	app.UsageText = "-k key -d url [-v version] [-ns]"
 	app.Description = "A hashpass-compatible stateless password manager/genrator"
 	app.HideVersion = true
 	app.Authors = []cli.Author{
@@ -59,9 +77,15 @@ func main() {
 		},
 
 		cli.StringFlag{
-			Name:        "domain, d",
-			Usage:       "The domain to use",
-			Destination: &domain,
+			Name:        "url, u",
+			Usage:       "The url to use",
+			Destination: &mUrl,
+		},
+
+		cli.BoolFlag{
+			Name:        "nostrip, ns",
+			Usage:       "Don't extract domain if a complete link is provided",
+			Destination: &nostrip,
 		},
 		cli.StringFlag{
 			Name:        "vesion, v",
@@ -72,11 +96,19 @@ func main() {
 	}
 
 	app.Action = func(c *cli.Context) error {
-		if key != "" && domain != "" {
+
+		// Check if necessary parameters are provided
+		if key != "" && mUrl != "" {
+
+			// Check if the provided url has to be stripped (i.e. extract the domain from a link
+			if !nostrip {
+				mUrl = stripUrl(mUrl)
+			}
+
 			if version != "" {
-				fmt.Println(generatePass(key, domain, version))
+				fmt.Println(generatePass(key, mUrl, version))
 			} else {
-				fmt.Println(generatePass(key, domain))
+				fmt.Println(generatePass(key, mUrl))
 			}
 		} else {
 			cli.ShowAppHelp(c)
